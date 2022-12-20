617b3a43da6887da483b0a2240414bbc8aeb9263
therogersak <therogersak@gmail.com>
2022-12-20 15:40:05 +0530

    ⚰️ remove dead code

--- src/Components/Home/MoviesRow/MoviesRow.js
+++ src/Components/Home/MoviesRow/MoviesRow.js
@@ -1,8 +1,6 @@
 import React from "react";
 import "./MoviesRow.css";
 import StarIcon from "@mui/icons-material/Star";
-import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
-import FavoriteIcon from "@mui/icons-material/Favorite";
 import { useDispatch } from "react-redux";
 import { favAdd } from "../../../features/favSlice";
 import { Link } from "react-router-dom";
@@ -11,7 +9,6 @@ const base_url = "https://image.tmdb.org/t/p/original";
 function MoviesRow({ img, id, title, type, release_date, rate, grid }) {
   const year = new Date(release_date);
 
-  const [icon, setIcon] = React.useState(<FavoriteBorderIcon />);
   const dispatch = useDispatch();
 
   const truncate = (string, num) => {
@@ -19,7 +16,6 @@ function MoviesRow({ img, id, title, type, release_date, rate, grid }) {
   };
 
   const FavHanlder = () => {
-    setIcon(<FavoriteIcon />);
     dispatch(
       favAdd({
         id: id,
@@ -38,10 +34,6 @@ function MoviesRow({ img, id, title, type, release_date, rate, grid }) {
         <Link to={`/${type}/${id}`}>
           <img src={`${base_url}${img}`} />
         </Link>
-        <div className="fav" onClick={() => FavHanlder()}>
-          {icon}
-        </div>
-
         <div className={grid ? "movies__info inn" : "movies_info"}>
           <div className="movie__name">
             <h3>{truncate(title, 18)}</h3>
